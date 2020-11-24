8c53c266235f69409a5504c4b3c2e5897fca4e5f
akshaygodse13 <72199103+akshaygodse13@users.noreply.github.com>
2020-11-24 18:04:08 +0530

    Update first.s

--- first.s
+++ first.s
@@ -14,12 +14,12 @@
 __main  FUNCTION
 
 ; r is started from 1 to 2400 value as it will take 8 loops 2400/360=8 
-		VLDR.F32 s22,=1.0				;radius starting value
-		VMOV.F32 R0,S22
-		VLDR.F32 S26,=3000.0				
-		VMOV.F32 R1,S26
-		VLDR.F32 S27,=3000.0				
-		VMOV.F32 R2,S27
+		VLDR.F32 S17,=1.0				;radius starting value
+		VMOV.F32 R0,S17
+		VLDR.F32 S18,=3000.0				
+		VMOV.F32 R1,S18
+		VLDR.F32 S19,=3000.0				
+		VMOV.F32 R2,S19
 		BL printMessage
 		
 		BL __DRAWSPIRAL					
@@ -32,29 +32,29 @@ __DRAWSPIRAL FUNCTION
 		VLDR.F32 S21,=360.0					;max angle value
 		VLDR.F32 S25,=2400.0  				;max radius value
 		VLDR.F32 S24,=1						;radius increment
-		VLDR.F32 S9,=0.0 					;This isthe input for sine and cosine
-CORDC	VMOV.F32 R0,S9
+		VLDR.F32 S11,=0.0 					;This isthe input for sine and cosine
+CORDC	VMOV.F32 R0,S11
 		;BL printMsg		
 		;BL printComma
 		BL __SINE							
-		VMUL.F32 S5,S5,S22					
-		VADD.F32 S5,S5,S27					
+		VMUL.F32 S5,S5,S17					
+		VADD.F32 S5,S5,S19					
 		VMOV.F32 R0,S5			;x value
 		BL printMsg		
 		BL printComma
 		BL __COSINE							
-		VMUL.F32 S5,S5,S22					
-		VADD.F32 S5,S5,S26					
+		VMUL.F32 S5,S5,S17					
+		VADD.F32 S5,S5,S18					
 		VMOV.F32 R0,S5			;y value
 		BL printMsg
 		BL printNextline
-		VADD.F32 S9,S9,S20					;angle increement by 1
-		VADD.F32 S22,S22,S24                 ;radius increment by 0.5
-		VCMP.F32 S9,S21						;for initializing angle to 0
+		VADD.F32 S11,S11,S20					;angle increement by 1
+		VADD.F32 S17,S17,S24                 ;radius increment by 0.5
+		VCMP.F32 S11,S21						;for initializing angle to 0
 		VMRS APSR_nzcv, FPSCR
 		IT EQ;
-		VMOVEQ.f32 S9,S20;
-		VCMP.F32 S22,S25						;max Radius reaches 2400
+		VMOVEQ.f32 S11,S20;
+		VCMP.F32 S17,S25						;max Radius reaches 2400
 		VMRS APSR_nzcv, FPSCR
 		BNE CORDC		
 		
@@ -62,20 +62,20 @@ CORDC	VMOV.F32 R0,S9
 		
 		ENDFUNC
 
-__SINE FUNCTION			;Here input is S9 and output is S5 = sin()
+__SINE FUNCTION			;Here input is S11 and output is S5 = sin()
         VLDR.F32 S1,=90.0
-        VMOV.F32 S0,S9
-        VCMP.F32 S9,S1
+        VMOV.F32 S0,S11
+        VCMP.F32 S11,S1
 		VMRS APSR_nzcv, FPSCR
 		BLT SINPI
         VLDR.F32 S1,=180.0		
-		VSUB.F32 S0,S1,S9
+		VSUB.F32 S0,S1,S11
         VLDR.F32 S1,=270.0
-		VCMP.F32 S9,S1
+		VCMP.F32 S11,S1
 		VMRS APSR_nzcv, FPSCR
         BLT SINPI
 		VLDR.F32 S1,=360.0
-        VSUB.F32 S0,S9,S1   				;Stores angle range from -PI/2 to PI/2
+        VSUB.F32 S0,S11,S1   				;Stores angle range from -PI/2 to PI/2
 SINPI   VLDR.F32 S7,=3.14159265
 		VLDR.F32 S8,=180.0
 		VLDR.F32 S6,=1.0
@@ -96,7 +96,7 @@ MULSIN	VMUL.F32 S3,S3,S2					;S3*(-x^2)
 		VADD.F32 S5,S5,S3					
 		SUB R1,R1,#1						
 		CMP R1,#0
-		BNE MULSIN							;Branch to next term calculation
+		BNE MULSIN							;Branch for next Term calculation
 		                                    ;S5 = SIN(X) is the final answer
 		VMOV.F32 R0,S5
 		BX lr		
@@ -108,7 +108,7 @@ __COSINE FUNCTION
 		VLDR.F32 S8,=180.0
 		VLDR.F32 S6,=1.0
 		MOV R1,#20         					;NUMBER OF TERMS IN COMPUTATION
-		VMUL.F32 S1,S9,S7
+		VMUL.F32 S1,S11,S7
 		VDIV.F32 S1,S1,S8					;x=PI*(theta)/180
 		VMUL.F32 S2,S1,S1					;x^2
 		VNEG.F32 S2,S2						;-x^2		
